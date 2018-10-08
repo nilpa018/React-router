6e013dc75fe686676d536ce0ba22560a1595d37c
Loïc Marchand <nilpa018@yahoo.fr>
2018-10-08 15:46:00 +0200

    Quete react router

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
+import "./App.css";
+import Home from './Home';
+import History from './History';
+
+class App extends Component {
+    render() { 
+        return (
+            <BrowserRouter>
+              <div>
+                <NavLink exact to="/" activeClassName="selected"> Home </NavLink>
+                <NavLink to="/notre-histoire" activeClassName="selected"> History </NavLink>
+
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route path="/notre-histoire" component={History} />
+                </Switch>
+              </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
